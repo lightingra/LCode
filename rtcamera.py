46773a74b2b985d31e93a193a2c07036102c232e
lightingra <45381286+lightingra@users.noreply.github.com>
2021-09-26 02:54:04 +0800

    git test 2

--- rtcamera.py
+++ rtcamera.py
@@ -1,5 +1,25 @@
 import cv2
+from threading import Thread
+import queue
+
+class RTCamera(Thread):
+
+    def run(self) -> None:
+        while True:
+            flag, img = self._cap.read()
+
+            if flag:
+                pass
+
+    def start(self, id) -> None:
+        self._cap = cv2.VideoCapture(id)
+        self._imgQ = queue.Queue()
+
+    def __init__(self):
+        Thread.__init__()
+
+        self.daemon = True
+
 
 if __name__ == '__main__':
     print('test')
-    
